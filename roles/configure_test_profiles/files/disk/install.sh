bdf1246c0e028517689f804fab4ed1e462b676f8
Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>
2024-12-02 16:46:57 +0100

    test-profiles/disk: add simple disk benchmark
    This test is performing a simple disk benchmark, in a VM or an
    hypervisor.
    
    Signed-off-by: Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>

---
+++ roles/configure_test_profiles/files/disk/install.sh
@@ -0,0 +1,21 @@
+# Copyright (C) 2024 Savoir-faire Linux, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+#!/bin/sh
+# Auto-generated install.sh script for starting/helping the test profile creation process...
+
+if which fio>/dev/null 2>&1 ;
+then
+	echo 0 > ~/install-exit-status
+else
+	echo "fatal: fio is not found on the system! This test profile needs a working fio installation in the PATH"
+	echo 2 > ~/install-exit-status
+fi
+
+echo "#!/bin/bash
+    fio --name=random-writers --ioengine=libaio --iodepth=4 --rw=randwrite --bs=32k --direct=0 --size=64m --numjobs=4 --runtime=60 --time_based \$@ > \$LOG_FILE 2>&1
+    echo \$? > ~/test-exit-status" > ~/disk
+chmod +x ~/disk
+
+# Check out the `phoronix-test-suite debug-run` command when trying to
+# debug your install/run behavior
