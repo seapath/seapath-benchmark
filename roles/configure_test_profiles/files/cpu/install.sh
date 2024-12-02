904223f63bd6a702387de369f1d937b14cf2021c
Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>
2024-12-02 15:58:42 +0100

    test-profiles/cpu: add simple cpu benchmark
    This test is performing a simple CPU benchmark, in a VM or an
    hypervisor.
    
    Signed-off-by: Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>

---
+++ roles/configure_test_profiles/files/cpu/install.sh
@@ -0,0 +1,21 @@
+# Copyright (C) 2024 Savoir-faire Linux, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+#!/bin/sh
+# Auto-generated install.sh script for starting/helping the test profile creation process...
+
+if which sysbench>/dev/null 2>&1 ;
+then
+	echo 0 > ~/install-exit-status
+else
+	echo "fatal: sysbench is not found on the system! This test profile needs a working sysbench installation in the PATH."
+	echo 2 > ~/install-exit-status
+fi
+
+echo "#!/bin/bash
+    sysbench cpu run --threads=500 --time=60 \$@ > \$LOG_FILE 2>&1
+    echo \$? > ~/test-exit-status" > ~/cpu
+chmod +x ~/cpu
+
+# Check out the `phoronix-test-suite debug-run` command when trying to
+# debug your install/run behavior
